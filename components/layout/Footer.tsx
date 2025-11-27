1d097f8e4c2cfb5dfe4e466e5ddf80cfd1de0732
jowy2211 <jowy2211@gmail.com>
2025-11-27 13:47:14 +0700

    Fix: Footer component

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 bg-white border-t-4 border-stone-800 z-30">
+    <footer className="fixed bottom-0 left-0 right-0 mt-1 bg-white border-t border-stone-800 z-30">
       <div className="max-w-7xl mx-auto px-4 py-2">
-        <p className="text-center text-s text-gray-600 font-medium">
+        <p className="text-center text-xs text-gray-600 font-medium">
           © 2025 ClarityHub. Created with ❤️ & ☕. All rights reserved.
         </p>
       </div>
